e9ccca625a4af00ea544e5c1d61a5a19ebce6def
ly <lingyuwhu@outlook.com>
2019-05-13 11:16:49 +0800

    * 修改公共函数

--- client/js/comm.js
+++ client/js/comm.js
@@ -1,9 +1,11 @@
-// 检查手机号码格式
-function checkPhoneNo(phone){
+// 正则-判断手机号码格式
+function checkMobile(phone){
     var reg = /^1[3|4|5|8]\d{9}$/;
-    if((reg.test(phone))){
-        return true;
-    }else{
-        return false;
-    }
+    return reg.test(phone);
+}
+
+// 正则-判断银行卡号(数字16-19)
+function checkCard(card) {
+	var reg = /^\d{16,19}$/g;
+	return reg.test(card);
 }
