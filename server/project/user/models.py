431e1c6b753f8d5f7ddc255aa3b9cb9b6c578aec
LINGYU <LINGYU@df8000.com>
2020-08-27 17:45:59 +0800

    * 修改用户模型

--- server/project/user/models.py
+++ server/project/user/models.py
@@ -1,8 +1,16 @@
 from db import db
-from sqlalchemy import Column, Integer, Text, String
+import datetime
+from sqlalchemy import Column, Integer, Text, String, DateTime
 
 
 class User(db.Model):
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(length=10), nullable=False)
     password = Column(String(length=255), nullable=False)
+    createdate = Column(DateTime,
+                        nullable=False,
+                        default=datetime.datetime.now())
+    gender = Column(Integer, default=0)
+    birthday = Column(DateTime, default=datetime.datetime.now())
+    email = Column(String(length=255))
+    mobile = Column(String(length=11))
