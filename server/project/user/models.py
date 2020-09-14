6de7963b7902d24ecd406f2ba0f8960fb7c06dd5
LINGYU <LINGYU@df8000.com>
2020-09-14 14:19:04 +0800

    * 模型添加family

--- server/project/user/models.py
+++ server/project/user/models.py
@@ -1,11 +1,15 @@
 from db import db
 import datetime
-from sqlalchemy import Column, Integer, Text, String, DateTime
+from sqlalchemy import Table, Column, UniqueConstraint, Integer, Text, String, DateTime, Boolean, ForeignKey
+# from sqlalchemy.orm import relationship
 
 
 class User(db.Model):
+    '''
+    用户信息表
+    '''
     id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String(length=10), nullable=False)
+    name = Column(String(length=20), nullable=False)
     password = Column(String(length=255), nullable=False)
     createdate = Column(DateTime,
                         nullable=False,
@@ -14,3 +18,31 @@ class User(db.Model):
     birthday = Column(DateTime, default=datetime.datetime.now())
     email = Column(String(length=255))
     mobile = Column(String(length=11))
+
+
+class Family(db.Model):
+    '''
+    家庭信息表
+    '''
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(length=20), nullable=False)
+    description = Column(String(length=255), nullable=False)
+    createdate = Column(DateTime,
+                        nullable=False,
+                        default=datetime.datetime.now())
+    address = Column(String(length=255), nullable=False)
+
+
+class UserFamilyRelationship(db.Model):
+    '''
+    家庭成员信息表
+
+    一个家庭可以包含多个成员
+
+    一个用户可以属于多个家庭
+    '''
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    userid = Column(Integer, nullable=False)
+    familyid = Column(Integer, nullable=False)
+    userisadmin = Column(Boolean)
+    UniqueConstraint(userid, familyid)
